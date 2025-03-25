e78c89ee8d4937349485b4c7dea4fa9f8a03ef90
Abhishek Atole <85939904+Abhishek-Atole@users.noreply.github.com>
2025-03-25 10:44:42 +0000

    Commits

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -6,7 +6,7 @@ export default function Certificates() {
     {
       title: 'C Programming',
       issuer: 'LinkedIn Learning',
-      image: 'images/c prog.jpg',
+      image: 'https://github.com/Abhishek-Atole/Portfolio/blob/main/images/C%2B%2BProgram.jpg',
       link: 'https://www.linkedin.com/learning/certificates/8cbed9814a163b91f00feac525839739085741dd84c32f2e3e6565c290c2420e?trk=share_certificate'
     },
     {
