8fa1d228f072df9c92c36bc0695bef5c8ccb8b4d
Abhishek Atole <85939904+Abhishek-Atole@users.noreply.github.com>
2025-03-25 12:07:15 +0000

    Update image paths and move images to public directory

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -6,37 +6,37 @@ export default function Certificates() {
     {
       title: 'C Programming',
       issuer: 'LinkedIn Learning',
-      image: 'dist/assets/c Embed.jpg',
+      image: '/images/c%20prog.jpg', // Updated path
       link: 'https://www.linkedin.com/learning/certificates/8cbed9814a163b91f00feac525839739085741dd84c32f2e3e6565c290c2420e?trk=share_certificate'
     },
     {
       title: 'Master C++ Programming',
       issuer: 'Udemy',
-      image: 'images/C++Program.jpg',
+      image: '/images/C++Program.jpg', // Updated path
       link: 'https://www.udemy.com/certificate/UC-5f7e3c2a-9296-46a2-af0f-d3e7b7b2ccc1/'
     },
     {
       title: 'C Programming for Embedded Applications',
       issuer: 'Linkedin Learning',
-      image: 'images/c Embed.jpg',
+      image: '/images/c%20Embed.jpg', // Updated path
       link: 'https://www.linkedin.com/learning/certificates/446728be8710513dd52a7b731e6152e99033d17d30cdd82f2330ccf7a1344b9c?trk=share_certificate'
     },
     {
       title: 'Getting Started with Linux',
       issuer: 'Linkedin Learning',
-      image: 'images/linux.jpg',
+      image: '/images/linux.jpg', // Updated path
       link: 'https://www.linkedin.com/learning/paths/getting-started-with-linux'
     },
     {
       title: 'Heterogeneous Parallel Programming using CUDA and OpenCL',
       issuer: 'Cisco',
-      image: 'images/HPP.jpg',
+      image: '/images/HPP.jpg', // Updated path
       link: 'https://astromedicomp.org/Certificate/StudentCertificate.php?cuid=HPP-2025-ILTOCF649M'
     },
     {
       title: 'Cybersecurity Foundation',
       issuer: 'Cisco Netacademy',
-      image: 'images/cyber.png',
+      image: '/images/cyber.png', // Updated path
       link: 'https://www.credly.com/badges/c7ee13ea-2f69-4cae-815e-dd15b6e068ad/public_url'
     },
     {
@@ -45,7 +45,7 @@ export default function Certificates() {
       image: 'https://images.unsplash.com/photo-1633356122102-3fe601e05bd2?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80',
       link: '#'
     }
-];
+  ];
   return (
     <section className="py-20 bg-dark">
       <div className="container mx-auto max-w-6xl px-4">
